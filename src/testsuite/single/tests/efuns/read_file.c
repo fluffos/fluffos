ec0a27e0f638114da598a32573a139ff8576fc2a
Yucong Sun <sunyucong@gmail.com>
2013-03-25 02:24:07 -0700

    Rewrite read_file() to use a single static buffer.

--- src/testsuite/single/tests/efuns/read_file.c
+++ src/testsuite/single/tests/efuns/read_file.c
@@ -5,6 +5,8 @@ void do_tests() {
     ASSERT(!read_file("/single", 10, 10));
     ASSERT(read_file("/testfile") == foo);
     ASSERT(read_file("/testfile", 100, 100) == mid);
+    // if we are asking more lines than in the file, return whole file
+    ASSERT(read_file("/testfile", 100, 0x7fffffff) == mid);
     ASSERT(!read_file("/does_not_exist"));
     ASSERT(!read_file("/testfile", 10000, 1));
 }
