266bb293bcc9b8d7ab77e96584bac5592307ef62
Yucong Sun <sunyucong@gmail.com>
2013-03-30 02:59:40 -0700

    Make make test actually fail the build.
    Also improve the existing read_file tests.
    
    Conflicts:
    	src/testsuite/single/tests/efuns/read_file.c

--- src/testsuite/single/tests/efuns/read_file.c
+++ src/testsuite/single/tests/efuns/read_file.c
@@ -1,10 +1,13 @@
 void do_tests() {
     string foo = read_bytes("/testfile");
-    string mid = implode(explode(foo, "\n")[99..198], "\n") + "\n";
-    
+    string mid = implode(explode(foo, "\n")[9..18], "\n") + "\n";
+    string all = implode(explode(foo, "\n")[9..<1], "\n") + "\n";
+
     ASSERT(!read_file("/single", 10, 10));
-    ASSERT(read_file("/testfile") == foo);
-    ASSERT(read_file("/testfile", 100, 100) == mid);
+    ASSERT(foo == read_file("/testfile"));
+    ASSERT_EQ(mid, read_file("/testfile", 10, 10));
+    // if we are asking more lines than in the file, return whole file
+    ASSERT(all == read_file("/testfile", 10, 0x7fffffff));
     ASSERT(!read_file("/does_not_exist"));
     ASSERT(!read_file("/testfile", 10000, 1));
 }
