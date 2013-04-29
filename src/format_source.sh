b678fe73ec8c603618e9db7b331c9b2eda3e525a
Yucong Sun <sunyucong@gmail.com>
2013-04-28 23:42:15 -0700

    Use astyle to automatically format source.

---
+++ src/format_source.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# astyle option file
+# http://astyle.sourceforge.net/astyle.html#_Options
+
+ASTYLE="astyle \
+--mode=c \
+--style=kr \
+--indent=spaces=2 \
+--indent-switches \
+--pad-oper \
+--pad-header \
+--unpad-paren \
+--keep-one-line-blocks \
+--keep-one-line-statements \
+--convert-tabs \
+--align-pointer=name \
+--indent-col1-comments \
+--min-conditional-indent=0 \
+--lineend=linux \
+"
+
+for f in `find . -regex ".*\.[ch]$" | grep -v "compat" | grep -v "testsuite" | grep -v "_spec"`; do
+  echo $ASTYLE $f
+  $ASTYLE $f
+done
