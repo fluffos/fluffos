6e3f7b64ab0e56b833c8f3a69b05847ebeab3dd9
Zoilder <zoilder.rlmud@gmail.com>
2013-05-07 18:21:51 +0200

    Trim, LTrim & RTrim efuns

---
+++ src/packages/trim_spec.c
@@ -0,0 +1,14 @@
+/*
+  Author: Zoilder (RL-Mud)
+  Date: 04/05/2013
+  Description: Functions for trim system.
+*/
+#include "spec.h"
+
+/*
+  Parameter 1: Is mixed, so if it isn't string, it returns "".
+  Parameter 2: Could be string, with a list of characters leading / trailing to remove. Void use isspace function.
+*/
+string trim(mixed, string | void);
+string ltrim(mixed, string | void);
+string rtrim(mixed, string | void);
