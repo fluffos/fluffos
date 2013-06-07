f269acc2bbebcbc8a73e0ac0fb8768576d827d84
Yucong Sun <sunyucong@gmail.com>
2013-06-06 20:36:06 -0700

    format source

--- src/packages/trim.c
+++ src/packages/trim.c
@@ -21,7 +21,7 @@ static inline std::string &ltrim(std::string &s, const std::string &charset)
     s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
   } else {
     int pos = s.find_first_not_of(charset);
-    if(pos == -1)
+    if (pos == -1)
       s.clear();
     else
       s.erase(s.begin(), s.begin() + pos);
