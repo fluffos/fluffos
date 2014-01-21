5d12263ffdd4ba1c3a58e16ac1d4bdd93f7e2461
Yucong Sun <sunyucong@gmail.com>
2014-01-20 22:43:23 -0800

    Use clang-format to format source

--- src/format_source.sh
+++ src/format_source.sh
@@ -3,23 +3,23 @@
 # astyle option file
 # http://astyle.sourceforge.net/astyle.html#_Options
 
-ASTYLE="astyle \
---mode=c \
---style=kr \
---indent=spaces=2 \
---indent-switches \
---pad-oper \
---pad-header \
---unpad-paren \
---keep-one-line-blocks \
---keep-one-line-statements \
---convert-tabs \
---align-pointer=name \
---indent-col1-comments \
---min-conditional-indent=0 \
---lineend=linux \
-"
+#ASTYLE="astyle \
+#--mode=c \
+#--style=kr \
+#--indent=spaces=2 \
+#--indent-switches \
+#--pad-oper \
+#--pad-header \
+#--unpad-paren \
+#--keep-one-line-blocks \
+#--keep-one-line-statements \
+#--convert-tabs \
+#--align-pointer=name \
+#--indent-col1-comments \
+#--min-conditional-indent=0 \
+#--lineend=linux \
+#"
 
 for f in `find . -regextype posix-extended -regex ".*\.(cc|h)$" | grep -v "compat" | grep -v "testsuite" `; do
-  if [ -s $f ]; then $ASTYLE $f; fi
+  if [ -s $f ]; then $1 -i --style="{BasedOnStyle: google, ColumnLimit: 0}" $f; fi
 done
