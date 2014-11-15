d4a172a99e72db47b1cd38f9396514bc7d52d7f5
Carl Lucas <carl.lucas@gmail.com>
2014-11-15 08:26:32 -0600

    remove remnants of POSIX_TIMERS option

--- src/posix_timers.h
+++ src/posix_timers.h
@@ -3,10 +3,8 @@
 
 void sigalrm_handler(int, siginfo_t *, void *);
 
-#ifdef POSIX_TIMERS
 void init_posix_timers(void);
 void posix_eval_timer_set(LPC_INT micros);
 LPC_INT posix_eval_timer_get(void);
-#endif
 
 #endif
