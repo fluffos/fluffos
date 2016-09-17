73c01001785e442ebb67c496f2f6e3d274a87c41
cartercheng@gmail.com <cartercheng@gmail.com>
2016-09-17 20:25:03 +0800

    new otable implementation.

--- src/vm/internal/otable.h
+++ src/vm/internal/otable.h
@@ -1,15 +1,39 @@
+
 #ifndef OTABLE_H
 #define OTABLE_H
 
-/*
- * otable.c
- */
-void init_otable(void);
-void enter_object_hash(struct object_t *);
-void remove_object_hash(struct object_t *);
-void remove_precompiled_hashes(char *);
-struct object_t *lookup_object_hash(const char *);
-int show_otable_status(struct outbuffer_t *, int);
-struct array_t *find_ch_n(const char *s);
+#include<list>
+#include<string>
+#include<unordered_map>
+
+//change this into a singleton?
+//or make the otable and callout tables a part of a new runtime data structure?
+//change basename to use std::string? 
+
+
+class ObjectTable
+{
+public:
+    static void init(); 
+    static ObjectTable* getInstance();
+
+    void insert(struct object_t *);
+    void remove(struct object_t *);
+    object_t *find(const char *);
+    int show_otable_status (outbuffer_t *, int);
+    struct array_t *children(const char *);
+private:
+	
+	ObjectTable() {}
+	std::string basename(const char *full);
+
+	static ObjectTable* instance_;
+
+    std::unordered_map< std::string, struct object_t* > objects_;
+    std::unordered_map< std::string, std::list<struct object_t*> > children_;
+    long searches_, found_;
+    long userLookups_;
+    long userFound_;
+};
 
 #endif
