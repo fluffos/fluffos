4ab1830ccddaf64eebd456ee22a492cc1c0bd463
Yucong Sun <sunyucong@gmail.com>
2017-01-05 21:44:20 -0800

    Fix code and test issue introduced by get_include_path()

--- src/testsuite/single/tests/compiler/get_include_path.c
+++ src/testsuite/single/tests/compiler/get_include_path.c
@@ -2,14 +2,14 @@ void do_tests() {
     object ob;
 
     ob = load_object("/clone/mgip1");
-    ASSERT(ob->doit() == 1);
+    ASSERT_EQ(1, ob->doit());
 
     ob = load_object("/clone/mgip2");
-    ASSERT(ob->doit() == 2);
+    ASSERT_EQ(2, ob->doit());
 
     ob = load_object("/clone/mgip3");
-    ASSERT(ob->doit() == 3);
+    ASSERT_EQ(3, ob->doit());
 
     ob = load_object("/clone/mgip4");
-    ASSERT(ob->doit() == 3);
+    ASSERT_EQ(3, ob->doit());
 }
