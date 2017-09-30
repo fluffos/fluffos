9fc5c75de45c5e68fefd84f5b2636f9bf79edf29
Carter Cheng <cartercheng@gmail.com>
2017-09-30 23:26:01 +0900

    replaced otable.cc with newer version based on std::unordered_map.

--- src/vm/internal/otable.h
+++ src/vm/internal/otable.h
@@ -1,15 +1,51 @@
 #ifndef OTABLE_H
 #define OTABLE_H
 
-/*
- * otable.c
- */
-void init_otable(void);
-void enter_object_hash(struct object_t *);
-void remove_object_hash(struct object_t *);
-void remove_precompiled_hashes(char *);
-struct object_t *lookup_object_hash(const char *);
-int show_otable_status(struct outbuffer_t *, int);
-struct array_t *find_ch_n(const char *s);
+#include<cassert>
+#include<list>
+#include<memory>
+#include<string>
+#include<unordered_map>
+// #include "base/std.h"
+// #include "vm/internal/base/object.h"
+
+#ifdef TESTING
+struct object_t
+{
+public:
+    std::string name;
+
+    object_t(std::string const & s)
+    :name(s) {}
+};
+#endif
+
+class ObjectTable /* final */
+{
+public:
+    using K = std::string;
+    using V = object_t*;
+    using L = std::vector<V>;
+    using M1 = std::unordered_map<K,V>;
+    using M2 = std::unordered_map<K,L>;
+    using S = std::shared_ptr<ObjectTable>;
+
+    static S get();
+    bool insert(K k, V v);
+    V find(K const & k);
+    L children(K const & k);
+    bool remove(K const & k);
+    int showStatus(outbuffer_t *out, int verbose);
+
+    K basename(K k);
+private:
+    static S instance_;
+
+    ObjectTable();
+    
+    M1 objects_;
+    M2 children_;
+
+};
 
 #endif
--- src/vm/internal/vm.cc
+++ src/vm/internal/vm.cc
@@ -12,7 +12,6 @@
 #include "vm/internal/eval_limit.h"
 #include "vm/internal/master.h"
 #include "vm/internal/simul_efun.h"
-#include "vm/internal/otable.h"             // for init_otable
 #include "vm/internal/base/apply_cache.h"   // for apply_cache_init
 #include "vm/internal/base/machine.h"       // for reset_machine
 #include "vm/internal/compiler/lex.h"       // for add_predefines, fixme!
@@ -70,7 +69,6 @@ void vm_init() {
   init_strings();     /* in stralloc.c */
   init_identifiers(); /* in lex.c */
   init_locals();      /* in compiler.c */
-  init_otable();      /* in otable.c */
 
   max_eval_cost = CONFIG_INT(__MAX_EVAL_COST__);
   set_inc_list(CONFIG_STR(__INCLUDE_DIRS__));
