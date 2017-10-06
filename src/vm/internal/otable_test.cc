835ff596cfeda37024d46f5ce7c161899a294f4a
Carter Cheng <cartercheng@gmail.com>
2017-10-06 20:40:26 +0900

    removed old commented out code.

--- src/vm/internal/otable_test.cc
+++ src/vm/internal/otable_test.cc
@@ -42,32 +42,6 @@ TEST(OTableTest, Operations) {
 }
 
 int main(int argc, char **argv) {
-    /*
-    auto t = ObjectTable::get();
-
-    std::cout << ObjectTable::get()->basename("/realms/silenus.c") << std::endl;
-    std::cout << ObjectTable::get()->basename("//realms/silenus.c") << std::endl;
-    std::cout << ObjectTable::get()->basename("/////realms/silenus.c") << std::endl;
-    std::cout << ObjectTable::get()->basename("/realms/silenus.c.c") << std::endl;
-    std::cout << ObjectTable::get()->basename("///realms/silenus.c.c.c") << std::endl;
-    std::cout << ObjectTable::get()->basename("/realms/silenus#123") << std::endl;
-    std::cout << ObjectTable::get()->basename("/realms/beek#4") << std::endl;
-    */
-    
-/*
-    t->insert("/realms/silenus#1", new object_t("/realms/silenus#1"));
-    t->insert("/realms/silenus#13", new object_t("/realms/silenus#13"));
-    t->insert("/realms/silenus#2", new object_t("/realms/silenus#2"));
-    t->insert("/realms/beek#123", new object_t("/realms/beek#123"));
-    std::cout << t->children("realms/silenus").size() << std::endl;
-    std::cout << t->children("realms/beek").size() << std::endl;
-    
-    std::cout << t->find("/realms/silenus#2")->obname << std::endl;
-    std::cout << t->remove("realms/descartes") << std::endl;
-    std::cout << t->remove("/realms/silenus#3") << std::endl;
-    std::cout << t->remove("/realms/silenus#2") << std::endl;
-    std::cout << t->children("realms/silenus").size() << std::endl;
-*/
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
