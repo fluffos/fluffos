65e03b2015219cd70ca5cd8991aa7adc18ed47f2
Shea690901 <Shea690901@users.noreply.github.com>
2017-11-23 23:25:22 +0100

    <fix>(vm/internal/master.cc) #403 (#405)
    * <fix>(master.cc) #403
    
    Signed-off-by: Shea690901 <ginny690901@hotmail.de>
    
    * <ref>(master.cc) #403
    
    better warnings for messages concerning author_file/domain_file
    (originally added with #311)
    
    Signed-off-by: Shea690901 <ginny690901@hotmail.de>

--- src/vm/internal/master.cc
+++ src/vm/internal/master.cc
@@ -96,9 +96,7 @@ void set_master(object_t *ob) {
 #if defined(PACKAGE_UIDS) || defined(PACKAGE_MUDLIB_STATS)
   int first_load = (!master_ob);
 #endif
-#ifdef PACKAGE_UIDS
   svalue_t *ret;
-#endif
 
   get_master_applies(ob);
   master_ob = ob; // from here on apply_master_ob returns -1 only as return from the apply
@@ -116,7 +114,8 @@ void set_master(object_t *ob) {
     if (ret == 0 || ret->type != T_STRING) {
         // we didn't got the expected value?
         // emit warning and fall back to old behavior
-      debug_message("%s() in the master file does not work, using 'BACKBONE'\n",
+      debug_message("%s() in the master file does not work, using 'BACKBONE' as fallback (see %s.4)\n",
+                    applies_table[APPLY_DOMAIN_FILE],
                     applies_table[APPLY_DOMAIN_FILE]);
       set_backbone_domain("BACKBONE");
     } else {
@@ -130,7 +129,8 @@ void set_master(object_t *ob) {
     if (ret == 0 || ret->type != T_STRING) {
         // we didn't got the expected value?
         // emit warning and fall back to old behavior
-      debug_message("%s() in the master file does not work, using 'NONAME'\n",
+      debug_message("%s() in the master file does not work, using 'NONAME' as fallback (see %s.4)\n",
+                    applies_table[APPLY_AUTHOR_FILE],
                     applies_table[APPLY_AUTHOR_FILE]);
       set_master_author("NONAME");
     } else {
@@ -163,7 +163,8 @@ void set_master(object_t *ob) {
     if (ret == 0 || ret->type != T_STRING) {
         // we didn't got the expected value?
         // emit warning and fall back to old behavior
-      debug_message("%s() in the master file does not work, using root_uid\n",
+      debug_message("%s() in the master file does not work, using root_uid as fallback (see %s.4)\n",
+                    applies_table[APPLY_AUTHOR_FILE],
                     applies_table[APPLY_AUTHOR_FILE]);
       set_master_author(master_ob->uid->name);
     } else {
@@ -186,7 +187,8 @@ void set_master(object_t *ob) {
     if (ret == 0 || ret->type != T_STRING) {
         // we didn't got the expected value?
         // emit warning and fall back to old behavior
-      debug_message("%s() in the master file does not work, using bb_uid\n",
+      debug_message("%s() in the master file does not work, using bb_ui as fallback (see %s.4)d\n",
+                    applies_table[APPLY_DOMAIN_FILE],
                     applies_table[APPLY_DOMAIN_FILE]);
       set_backbone_domain(backbone_uid->name);
     } else {
