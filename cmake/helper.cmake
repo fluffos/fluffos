c011676418b3e87c759a2fd353c914273f6d6fa0
Yucong Sun <sunyucong@gmail.com>
2019-01-17 14:33:51 -0800

    Support and require USE_JEMALLOC in non debug build

--- cmake/helper.cmake
+++ cmake/helper.cmake
@@ -19,3 +19,25 @@ function(print_target_properties tgt)
     endforeach(prop)
 
 endfunction(print_target_properties)
+
+function(print_iterface_target_properties tgt)
+  if(NOT TARGET ${tgt})
+    message("There is no target named '${tgt}'")
+    return()
+  endif()
+
+  # this list of properties can be extended as needed
+  set(CMAKE_PROPERTY_LIST INTERFACE_SOURCE_DIR INTERFACE_BINARY_DIR INTERFACE_COMPILE_DEFINITIONS
+    INTERFACE_COMPILE_OPTIONS INTERFACE_INCLUDE_DIRECTORIES INTERFACE_LINK_LIBRARIES)
+
+  message("Configuration for target ${tgt}")
+
+  foreach (prop ${CMAKE_PROPERTY_LIST})
+    get_property(propval TARGET ${tgt} PROPERTY ${prop} SET)
+    if (propval)
+      get_target_property(propval ${tgt} ${prop})
+      message (STATUS "${prop} = ${propval}")
+    endif()
+  endforeach(prop)
+
+endfunction(print_iterface_target_properties)
