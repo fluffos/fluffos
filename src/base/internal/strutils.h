731166d52b20730f9e31d1b2ec3fdbe57cfc9a39
Yucong Sun <fallentree@fb.com>
2019-10-25 16:30:10 -0700

    Enable C++17 for v2019

--- src/base/internal/strutils.h
+++ src/base/internal/strutils.h
@@ -1,25 +1,32 @@
+#ifndef STRUTILS_H
+#define STRUTILS_H
+
 #include <algorithm>
 #include <functional>
 #include <cctype>
 #include <locale>
 
+namespace {
+
+const auto fn_not_isspace = [](int c) {return !std::isspace(c);};
+
 // trim from start
-static inline std::string &ltrim(std::string &s) {
+inline std::string &ltrim(std::string &s) {
   s.erase(s.begin(),
-          std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
+          std::find_if(s.begin(), s.end(), fn_not_isspace));
   return s;
 }
 
 // trim from end
-static inline std::string &rtrim(std::string &s) {
+inline std::string &rtrim(std::string &s) {
   s.erase(
-      std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(),
+      std::find_if(s.rbegin(), s.rend(), fn_not_isspace).base(),
       s.end());
   return s;
 }
 
 // trim from both ends
-static inline std::string &trim(std::string &s) { return ltrim(rtrim(s)); }
+inline std::string &trim(std::string &s) { return ltrim(rtrim(s)); }
 
 inline bool starts_with(const std::string &big_str, const std::string &small_str) {
   return big_str.compare(0, small_str.length(), small_str) == 0;
@@ -29,3 +36,6 @@ inline bool ends_with(const std::string &str, const std::string &ending) {
   if (str.length() < ending.length()) return false;
   return str.compare(str.length() - ending.length(), ending.length(), ending) == 0;
 }
+
+}
+#endif // STRUTILS_H
