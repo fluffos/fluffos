8c7716e23795d96386af1b7276347806a38cbe01
Yucong Sun <fallentree@fb.com>
2019-10-25 16:49:42 -0700

    Always use inhouse filesystem impl

--- src/tools/preprocessor.hpp
+++ src/tools/preprocessor.hpp
@@ -14,17 +14,8 @@
 #include <cstdlib>
 #include <vector>
 
-#if defined(__cplusplus) && __cplusplus >= 201703L && defined(__has_include)
-#if __has_include(<filesystem>)
-#define GHC_USE_STD_FS
-#include <filesystem>
-namespace fs = std::filesystem;
-#endif
-#endif
-#ifndef GHC_USE_STD_FS
 #include "thirdparty/filesystem/filesystem.hpp"
 namespace fs = ghc::filesystem;
-#endif
 
 unsigned int whashstr(const char *s) {
   int i = 0;
