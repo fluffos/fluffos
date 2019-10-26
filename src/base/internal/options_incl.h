1ed36dc286ebf0fefa9f28d96d84eadc850dda37
Yucong Sun <sunyucong@gmail.com>
2019-10-26 01:40:31 -0700

    some IWYU fixes

--- src/base/internal/options_incl.h
+++ src/base/internal/options_incl.h
@@ -1,8 +1,8 @@
 // This is the file to use for getting access to options within base module.
 // We process this file to generate list of defines, so we can't use include guard.
 
-//#ifndef __OPTIONS_INCL_H__
-//#define __OPTIONS_INCL_H__
+// #ifndef __OPTIONS_INCL_H__
+// #define __OPTIONS_INCL_H__
 
 // IWYU pragma: begin_exports
 #include "base/internal/options_internal.h"
@@ -12,4 +12,4 @@
 #include "packages/packages.autogen.h"
 // IWYU pragma: end_exports
 
-//#endif /* __OPTIONS_INCL_H__ */
+// #endif /* __OPTIONS_INCL_H__ */
