30d131a374b1ed6d6f6b80d6d423d42f66970f0a
Yucong Sun <sunyucong@gmail.com>
2019-11-16 21:13:30 -0800

    Fix issues detecting libevent2.0

--- cmake/FindLibevent.cmake
+++ cmake/FindLibevent.cmake
@@ -37,7 +37,7 @@ find_path(LIBEVENT_INCLUDE_DIR
         )
 
 if(LIBEVENT_INCLUDE_DIR)
-    set(_version_regex "^#define[ \t]+EVENT__VERSION[ \t]+\"([^\"]+)\".*")
+    set(_version_regex "^#define[ \t]+_*EVENT_+VERSION[ \t]+\"([^\"]+)\".*")
     if(EXISTS "${LIBEVENT_INCLUDE_DIR}/event2/event-config.h")
         # Libevent 2.0
         file(STRINGS "${LIBEVENT_INCLUDE_DIR}/event2/event-config.h"
