d4007423690dc9918a0f0fca286fd7862e961b46
Yucong Sun <sunyucong@gmail.com>
2019-11-22 03:08:36 -0800

    Adding test verifying rename overwrite behavior

--- testsuite/single/tests/efuns/rename.c
+++ testsuite/single/tests/efuns/rename.c
@@ -1,10 +1,18 @@
 void do_tests() {
+    // basic case
     write_file("/rename_test", "Hmm.");
     rename("/rename_test", "/ren_test");
     ASSERT(file_size("/rename_test") == -1);
     ASSERT(read_file("/ren_test") == "Hmm.");
+    // move into directory
     rename("/ren_test", "/single");
     ASSERT(file_size("/ren_test") == -1);
     ASSERT(read_file("/single/ren_test") == "Hmm.");
     rm("/single/ren_test");
+    // Verify overwrite
+    write_file("/rename_test", "1234");
+    write_file("/rename_test1", "4321");
+    rename("/rename_test", "/rename_test1");
+    ASSERT_EQ(-1, file_size("/rename_test"));
+    ASSERT_EQ("1234", read_file("/rename_test1"));
 }
