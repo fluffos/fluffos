4a63ce96af08d06f324eefb5930824c8934ce8f4
Shea690901 <ginny690901@hotmail.de>
2019-11-23 15:50:06 +0100

    <feat>(shared_string): added fmt formatter
    This way one doesn't need to obtain the stored value of a shared string
    to use it as argument for any formatting function from libfmt.
    
    Signed-off-by: Shea690901 <ginny690901@hotmail.de>

--- src/base/internal/shared_string.hh
+++ src/base/internal/shared_string.hh
@@ -1,15 +1,28 @@
 /// @file shared_string.hh
 /// @brief storage class for shared strings
 /// @author René Müller
-/// @version 0.0.0
+/// @version 0.0.1
 /// @date 2019-07-21
 
 #ifndef _SHARED_STRING_HH_
 #define _SHARED_STRING_HH_
 
 #include <string>
+#include <fmt/format.h>
+
 #include "templates/shared_storage.hh"
 
 using shared_string = shared_storage<std::string>;
+
+namespace fmt {
+    // format shared_string as std::string
+    template <>
+        struct formatter<shared_string> : formatter<std::string> {
+            template <typename FormatContext>
+                std::string format(const shared_string &p, FormatContext &ctx) {
+                    return formatter<std::string>::format(*p, ctx);
+                }
+        };
+}
 
 #endif // SHARED_STRING_HH
