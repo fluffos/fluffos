f60879a29edfd14492155749e04d2b9b3536c43c
Yucong Sun <sunyucong@gmail.com>
2019-11-22 19:19:58 -0800

    Bundle crypt funcitons from musl, also fix #455.

---
+++ src/thirdparty/crypt/crypt.h
@@ -0,0 +1,19 @@
+#ifndef CRYPT_H
+#define CRYPT_H
+
+#include "include/crypt.h"
+
+#define weak __attribute__((__weak__))
+#define hidden __attribute__((__visibility__("hidden")))
+#define weak_alias(old, new) \
+	extern __typeof(old) new __attribute__((__weak__, __alias__(#old)))
+
+hidden char *__crypt_r(const char *, const char *, struct crypt_data *);
+
+hidden char *__crypt_des(const char *, const char *, char *);
+hidden char *__crypt_md5(const char *, const char *, char *);
+hidden char *__crypt_blowfish(const char *, const char *, char *);
+hidden char *__crypt_sha256(const char *, const char *, char *);
+hidden char *__crypt_sha512(const char *, const char *, char *);
+
+#endif
