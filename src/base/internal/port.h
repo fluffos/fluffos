f77e58a1ea7d81c9b1ff82a69e5461955531f269
Yucong Sun <sunyucong@gmail.com>
2019-11-25 22:08:29 -0800

    Improve initlization to detect UTF8 locale and call tzset()

--- src/base/internal/port.h
+++ src/base/internal/port.h
@@ -7,7 +7,7 @@
  * port.c
  */
 int64_t random_number(int64_t);
-long get_current_time(void);
+time_t get_current_time(void);
 const char *time_string(time_t);
 void get_usec_clock(long *, long *);
 long get_cpu_times(unsigned long *, unsigned long *);
