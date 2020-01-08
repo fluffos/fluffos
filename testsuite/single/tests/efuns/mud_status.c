e2623ecbd2dea0bf801a42ec9361709765ac477f
Shea690901 <Shea690901@users.noreply.github.com>
2020-01-08 06:08:28 +0100

    <fix>(otable.cc): fixes issue #556 (#557)
    * <fix>(otable.cc): fixes issue #556
    
    operator>> extracts formated input from a string_stream, not the whole
    string.
    
    Signed-off-by: Shea690901 <ginny690901@hotmail.de>
    
    * Use explicit move constructor
    
    * Update otable.cc
    
    Co-authored-by: Yucong Sun <sunyucong@gmail.com>

--- testsuite/single/tests/efuns/mud_status.c
+++ testsuite/single/tests/efuns/mud_status.c
@@ -1,4 +1,9 @@
 void do_tests() {
     ASSERT(stringp(mud_status()));
     ASSERT(stringp(mud_status(1)));
+
+    printf("\n\tmud_status(0):\n%O\n\n\tmud_status(1):\n%O\n\n\tmud_status(-1):\n%O\n\n",
+            mud_status(0),
+            mud_status(1),
+            mud_status(-1));
 }
