7729c74ea2159744602006abf450d4dd2c6b3a50
Shea690901 <ginny690901@hotmail.de>
2020-01-26 13:20:01 +0100

    <ref>(core/package): part 15
    Signed-off-by: Shea690901 <ginny690901@hotmail.de>

--- src/packages/core/telnet_ext.cc
+++ src/packages/core/telnet_ext.cc
@@ -29,7 +29,7 @@ void on_telnet_do_gmcp(interactive_t* ip) {
 void f_send_gmcp() {
   auto ip = current_object->interactive;
   if (ip && ip->telnet) {
-    telnet_subnegotiation(ip->telnet, TELNET_TELOPT_GMCP, sp->u.string, SVALUE_STRLEN(sp));
+    telnet_subnegotiation(ip->telnet, TELNET_TELOPT_GMCP, sp->u.string->c_str(), sp->u.string->size());
     flush_message(ip);
   }
   pop_stack();
@@ -42,12 +42,12 @@ void on_telnet_do_zmp(const char** argv, unsigned long argc, interactive_t* ip) 
   ip->iflags |= USING_ZMP;
 
   // Push the command
-  copy_and_push_string(argv[0]);
+  push_string(argv[0]);
 
   // Push the array
   array_t* arr = allocate_array(argc - 1);
   for (int i = 1; i < argc; i++) {
-    arr->item[i].u.string = string_copy(argv[i], "ZMP");
+    arr->item[i].u.string = std::string {argv[i]};
     arr->item[i].type = T_STRING;
     arr->item[i].subtype = STRING_MALLOC;
   }
@@ -61,11 +61,11 @@ void f_send_zmp() {
   auto ip = current_object->interactive;
 
   if (ip && ip->telnet) {
-    telnet_begin_zmp(ip->telnet, (sp - 1)->u.string);
+    telnet_begin_zmp(ip->telnet, (sp - 1)->u.string->c_str());
 
     for (int i = 0; i < sp->u.arr->size; i++) {
       if (sp->u.arr->item[i].type == T_STRING) {
-        telnet_zmp_arg(ip->telnet, sp->u.arr->item[i].u.string);
+        telnet_zmp_arg(ip->telnet, sp->u.arr->item[i].u.string->c_str());
       }
     }
 
