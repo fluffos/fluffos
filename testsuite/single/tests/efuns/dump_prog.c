06c00b41b22be7457d20a8b9b98ce615c834fd33
Shea690901 <ginny690901@hotmail.de>
2020-02-02 15:33:57 +0100

    <feat>(efun/dump_prog): full test
    Signed-off-by: Shea690901 <ginny690901@hotmail.de>

--- testsuite/single/tests/efuns/dump_prog.c
+++ testsuite/single/tests/efuns/dump_prog.c
@@ -1,3 +1,25 @@
+void internal(int flag) {
+    string fn;
+
+    switch (flag) {
+        case 1:
+            fn = "/PROG_DUMP_1";
+            break;
+        case 2:
+            fn = "/PROG_DUMP_2";
+            break;
+        case 3:
+            fn = "/PROG_DUMP_3";
+            break;
+        default:
+            dump_prog(this_object());
+            return;
+    }
+    dump_prog(this_object(), flag, fn);
+}
+
 void do_tests() {
-  write(dump_prog(this_object()));
+    for(int i = 0; i < 4; i++) {
+        internal(i);
+    }
 }
