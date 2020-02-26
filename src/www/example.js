c8d8c9b0275250e5bbf2926f7f43c0b0312f928a
Yucong Sun <sunyucong@gmail.com>
2020-02-26 00:15:53 -0800

    Optimize webterminal for mobile

--- src/www/example.js
+++ src/www/example.js
@@ -1,13 +1,46 @@
+/**
+ * Determine the mobile operating system.
+ *
+ * @returns {Boolean}
+ */
+function isMobile() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+  // Windows Phone must come first because its UA also contains "Android"
+  if (/windows phone/i.test(userAgent)) {
+    return true;
+  }
+
+  if (/android/i.test(userAgent)) {
+    return true;
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    return true;
+  }
+
+  return false;
+}
+
+const WebFontSize = 16;
+const MobileFontSize = 9;
 const term = new Terminal({
-  rows: 50,
-  cols: 120,
   convertEol: true,
   disableStdin: true,
   scrollback: 1024,
   tabStopWidth: 4,
   fontFamily: 'SFMono-Regular,Consolas,Liberation Mono,Menlo,monospace, "Microsoft YaHei", SimSun',
-  fontSize: 16
+  fontSize: screen.width > 500 ? WebFontSize : MobileFontSize
 });
+
+const unicode11Addon = new Unicode11Addon.Unicode11Addon();
+term.loadAddon(unicode11Addon);
+term.unicode.activeVersion = '11';
+
+const fitAddon  = new FitAddon.FitAddon();
+term.loadAddon(fitAddon);
+
 
 let history = [];
 let history_index = -1;
@@ -45,26 +78,45 @@ function new_ws(urlpath, protocol) {
   return new WebSocket(urlpath, protocol);
 }
 
+
+window.addEventListener('scroll', function(e) {
+  window.scrollTo(0,0);
+  e.preventDefault();
+  e.stopPropagation();
+});
+
 document.addEventListener("DOMContentLoaded", function () {
+  const el_container = document.getElementById('container');
   const el_terminal = document.getElementById('terminal');
   const el_input = document.getElementById('command');
+
+  if(isMobile()) {
+    el_container.style.height = "50vh";
+  } else {
+    el_container.style.height = "100vh";
+  }
+
   el_input.setAttribute("disabled", "disabled");
   el_input.style.fontFamily = term.getOption("fontFamily");
   el_input.style.fontSize = term.getOption("fontSize") + "px";
 
   term.open(el_terminal);
+  fitAddon.fit();
 
   let ws = new_ws(get_appropriate_ws_url(""), "ascii");
   const attachAddon = new AttachAddon.AttachAddon(ws);
   term.loadAddon(attachAddon);
+  el_input.focus();
 
   try {
     ws.onopen = function () {
+      term.write("\033[9999;1H");
       term.write("\r\n======== Connected.\r\n");
       el_input.removeAttribute("disabled");
       el_input.focus();
     };
     ws.onclose = function () {
+      term.write("\033[9999;1H");
       term.write("\r\n======== Connection Lost.\r\n");
       el_input.setAttribute("disabled", "disabled");
     };
