848c703d3eb0f857adcab7cb55cee49972161d34
oiuv <i@oiuv.cn>
2020-03-12 11:05:50 +0800

    update break_string sefun

--- testsuite/std/break_string.c
+++ testsuite/std/break_string.c
@@ -1,12 +1,6 @@
 // From MorgenGrauen MUDlib
 // Rewirtten by lonely for driver of fluffos v2019
-
-#define BS_LEAVE_MY_LFS         1
-#define BS_SINGLE_SPACE         2
-#define BS_BLOCK                4
-#define BS_NO_PARINDENT         8
-#define BS_INDENT_ONCE         16
-#define BS_PREPEND_INDENT      32
+#include "break_string.h"
 
 private string stretch(string str, int width)
 {
