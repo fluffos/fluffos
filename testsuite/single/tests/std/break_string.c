4a2f610a365f06d39d09492fc6a7c89aefb35c4e
oiuv <i@oiuv.cn>
2020-03-12 11:05:50 +0800

    update break_string sefun

--- testsuite/single/tests/std/break_string.c
+++ testsuite/single/tests/std/break_string.c
@@ -1,9 +1,4 @@
-#define BS_LEAVE_MY_LFS         1
-#define BS_SINGLE_SPACE         2
-#define BS_BLOCK                4
-#define BS_NO_PARINDENT         8
-#define BS_INDENT_ONCE         16
-#define BS_PREPEND_INDENT      32
+#include "/std/break_string.h"
 
 void do_tests() {
   ASSERT_EQ("This is a longer text.\n"
---
+++ testsuite/std/break_string.h
@@ -0,0 +1,11 @@
+#ifndef __BREAK_STRING_H
+#define __BREAK_STRING_H
+
+#define BS_LEAVE_MY_LFS         1
+#define BS_SINGLE_SPACE         2
+#define BS_BLOCK                4
+#define BS_NO_PARINDENT         8
+#define BS_INDENT_ONCE         16
+#define BS_PREPEND_INDENT      32
+
+#endif
