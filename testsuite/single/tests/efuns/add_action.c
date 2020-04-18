872543f180af1fe9ba8f0e83d33f409f85ad39e4
Yucong Sun <sunyucong@gmail.com>
2020-04-17 17:04:59 -0700

    testcase: UTF8 verb in add_action

--- testsuite/single/tests/efuns/add_action.c
+++ testsuite/single/tests/efuns/add_action.c
@@ -1,18 +1,22 @@
 int called;
 int xverb;
+int utf8verb;
 
 void do_tests() {
 #ifndef __NO_ADD_ACTION__
     object tp;
-    xverb = called = 0;
+    xverb = called = utf8verb = 0;
     SAVETP;
     enable_commands();
     add_action( (: called = 1 :), "foo");
     add_action( (: xverb = 1 :), "b", 1);
+    add_action( (: utf8verb = 1 :), "测试测试");
     RESTORETP;
     command("foo");
     command("bar");
-    ASSERT(called);
-    ASSERT(xverb);
+    command("测试测试");
+    ASSERT_EQ(1, called);
+    ASSERT_EQ(1, xverb);
+    ASSERT_EQ(1, utf8verb);
 #endif
 }
