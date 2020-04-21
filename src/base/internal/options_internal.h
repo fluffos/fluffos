b52997845b8311f18b54f82478acfa32074a59e8
Yucong Sun <sunyucong@gmail.com>
2020-04-21 00:40:08 -0700

    Raise default LARGEST_PRINTABLE_STRING to 65535

--- src/base/internal/options_internal.h
+++ src/base/internal/options_internal.h
@@ -117,7 +117,7 @@
  *   comm.c's add_message(). Instead of blindly making this value larger,
  *   your mudlib should be coded to not send huge strings to users.
  */
-#define LARGEST_PRINTABLE_STRING 8192
+#define LARGEST_PRINTABLE_STRING 65535
 
 /* PROFILE_FUNCTIONS: define this to be able to measure the CPU time used by
  *   all of the user-defined functions in each LPC object.  Note: defining
