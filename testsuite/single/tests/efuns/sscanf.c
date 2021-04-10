670053b8ec71b95b07cc0f0c28e30b30332bda4f
Yucong Sun <sunyucong@gmail.com>
2021-04-10 11:01:19 -0700

    Adding more test cases for sscanf()

--- testsuite/single/tests/efuns/sscanf.c
+++ testsuite/single/tests/efuns/sscanf.c
@@ -2,11 +2,27 @@ inherit "/inherit/tests";
 
 void do_tests() {
     string s;
-    
+    int num;
+
     ASSERT(catch(sscanf("you%", "%s%", s)));
     ASSERT(catch(sscanf("you%", "%su%", s)));
     sscanf("you%", "%s%%", s);
     ASSERT_EQ("you", s);
     sscanf("you%", "%su%%", s);
     ASSERT_EQ("yo", s);
+
+    sscanf("123sdkfwo1239405adsf", "%d", num);
+    ASSERT_EQ(123, num);
+
+    sscanf("+29375282", "%d", num);
+    ASSERT_EQ(29375282, num);
+
+    sscanf("  -19322", "%d", num);
+    ASSERT_EQ(-19322, num);
+
+    sscanf("FFFF", "%x", num);
+    ASSERT_EQ(65535, num);
+
+    sscanf("0x1293jsf", "%x", num);
+    ASSERT_EQ(4755, num);
 }
