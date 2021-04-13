e3f06c226f84989717d0aa5d102e7f045274404e
Yucong Sun <sunyucong@gmail.com>
2021-04-12 22:48:36 -0700

    Move some grammar rules to seperate file

--- src/compiler/internal/compiler.h
+++ src/compiler/internal/compiler.h
@@ -193,7 +193,7 @@ void init_locals(void);
 
 void save_file_info(int, int);
 int add_program_file(char *, int);
-void yyerror(const char *);
+void yyerror(const char *fmt, ...);
 void yywarn(const char *);
 char *the_file_name(const char *);
 void free_all_local_names(int);
