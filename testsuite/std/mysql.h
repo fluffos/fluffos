7736b005ed4307f1983888af579c4899862bb917
oiuv <i@oiuv.cn>
2021-05-08 20:53:39 +0800

    add DB

---
+++ testsuite/std/mysql.h
@@ -0,0 +1,13 @@
+/*****************************************************************************
+File name: mysql.h
+Description: mysql 数据库配置文件，DB 模块会调用初始化数据库连接
+*****************************************************************************/
+#ifndef MYSQL_H
+#define MYSQL_H
+
+#define DB_HOST     "127.0.0.1"
+#define DB_DATABASE "mud"
+#define DB_USERNAME "root"
+#define DB_PASSWORD "root"
+
+#endif
