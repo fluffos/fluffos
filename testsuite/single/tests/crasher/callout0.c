42fe8219ad2eda5623117803e5c55101385d2ad8
Yucong Sun <sunyucong@gmail.com>
2021-08-26 21:46:26 -0700

    Make sure call_out event are always executed on next avaiable gametick, even when delay is less than game tick interval.
    This behavior was always the intention. Also fix callout0 test to be more visible.

--- testsuite/single/tests/crasher/callout0.c
+++ testsuite/single/tests/crasher/callout0.c
@@ -1,6 +1,11 @@
+int executed = 1;
+
 void test() {
-    call_out("test", 0);
+    int h = call_out("test", 0);
+    ASSERT_EQ(0, find_call_out(h));
+    write(sprintf("callout0: %d\n", executed));
+    executed++;;
 }
 void do_tests() {
-    test();
+    call_out("test", 0.1);
 }
