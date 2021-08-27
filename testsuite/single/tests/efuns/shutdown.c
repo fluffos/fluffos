e077ca7f6f056925a3fe6d0ad68f18f9072b40e0
Yucong Sun <sunyucong@gmail.com>
2021-08-26 23:15:00 -0700

    adding output to make it clear tests are finished.

--- testsuite/single/tests/efuns/shutdown.c
+++ testsuite/single/tests/efuns/shutdown.c
@@ -1,9 +1,11 @@
 void do_the_nasty_deed() {
+    write("all tests finished, shutting down.\n");
     shutdown(0);
     ASSERT(0);
 }
 
 void do_tests() {
+    write("shutdown in 15 seconds.\n");
     // This one is hard to test :-)
     call_out( (: do_the_nasty_deed :), 15);
 }
