ea8f02fd7ad1d8bd4335aef92bbc0cf763644b54
Yucong Sun <sunyucong@gmail.com>
2021-09-08 10:57:31 -0400

    Switch strptime/strftime to glibc version.

--- src/packages/core/time.cc
+++ src/packages/core/time.cc
@@ -1,8 +1,8 @@
 #include "base/package_api.h"
 
 #include <chrono>
-#include <iomanip>
 #include <string>
+#include <iomanip>
 #include <sstream>
 
 #ifdef F_PERF_COUNTER_NS
@@ -105,40 +105,44 @@ void f_localtime(void) {
 #ifdef F_STRFTIME
 void f_strftime() {
   auto arg_time = sp->u.number;
-  auto arg_fmt = (sp - 1)->u.string;
+  const auto *arg_fmt = (sp - 1)->u.string;
 
   time_t lt = arg_time;
   if (lt <= 0) lt = get_current_time();
   struct tm res = {};
-  auto tm = localtime_r(&lt, &res);
-  if (!tm) {
+  const struct tm* res_tm = localtime_r(&lt, &res);
+  if (!res_tm) {
     error("Invalid time passed to strftime");
   }
 
-  std::ostringstream ss;
-  ss << std::put_time(tm, arg_fmt);
-  if (ss.fail()) {
-    error("strftime() failed.");
-  }
+  const auto max_string_length = CONFIG_INT(__MAX_STRING_LENGTH__);
+  char buf[max_string_length];
+  int size = strftime(buf, sizeof(buf), arg_fmt, res_tm);
+  buf[size] = '\0';
 
   pop_2_elems();
-  auto result = ss.str();
-  copy_and_push_string(result.c_str());
+  copy_and_push_string(buf);
 }
 #endif
 
 #ifdef F_STRPTIME
 void f_strptime() {
-  auto arg_timestr = (sp)->u.string;
-  auto arg_fmt = (sp - 1)->u.string;
+  const auto *arg_timestr = (sp)->u.string;
+  const auto *arg_fmt = (sp - 1)->u.string;
 
   struct tm res = {};
+#ifndef _WIN32
+  auto *p = strptime(arg_timestr, arg_fmt, &res);
+  if (p == nullptr) {
+    error("strptime() parse failed.");
+  }
+#else
   std::istringstream ss(arg_timestr);
   ss >> std::get_time(&res, arg_fmt);
-
   if (ss.fail()) {
     error("strptime() parse failed.");
   }
+#endif
   pop_2_elems();
   push_number(mktime(&res));
 }
