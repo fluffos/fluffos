324f4c9d710e1f01d10c066ac6ec67c7a91535e5
Yucong Sun <sunyucong@gmail.com>
2023-01-04 20:47:00 -0800

    source formatting

--- src/format_source.sh
+++ src/format_source.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
-for f in $(find . -type f -name "*.cc" | grep -v "thirdparty" | grep -v "compat" | grep -v "testsuite" ); do
+for f in $(find . -type f -name "*.cc" | grep -v "thirdparty" | grep -v "compat" | grep -v "testsuite" | grep -v "autogen" ); do
   if [ -s "$f" ]; then $1 -i $f; fi
 done
 
-for f in $(find . -type f -name "*.h" | grep -v "thirdparty" | grep -v "compat" | grep -v "testsuite" ); do
+for f in $(find . -type f -name "*.h" | grep -v "thirdparty" | grep -v "compat" | grep -v "testsuite" | grep -v "autogen" ); do
   if [ -s "$f" ]; then $1 -i $f; fi
 done
