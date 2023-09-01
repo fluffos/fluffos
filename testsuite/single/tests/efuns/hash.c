95a48a3076daacaa5fcd630c24d818939bb1dddc
Michael Programs <michael.programs@gmail.com>
2023-09-01 16:05:29 -0400

    revert single/tests/efuns/hash test structure

--- testsuite/single/tests/efuns/hash.c
+++ testsuite/single/tests/efuns/hash.c
@@ -1,10 +1,7 @@
 void do_tests() {
-
 #ifndef __PACKAGE_CRYPTO__
     write("PACKAGE_CRYPTO not defined: test not run.\n");
-    return;
-#endif
-
+#else
     ASSERT_EQ(hash("md5", "11111"), "b0baee9d279d34fa1dfd71aadb908c3f");
     ASSERT_EQ(hash("md5", "abc"), "900150983cd24fb0d6963f7d28e17f72");
     ASSERT_EQ(hash("md5", "something"), "437b930db84b8079c2dd804a71936b5f");
@@ -27,4 +24,5 @@ void do_tests() {
     ASSERT_EQ(sizeof(hash("sha384", "12345")), 96);
     ASSERT_EQ(hash("sha512", "12345"), "3627909a29c31381a071ec27f7c9ca97726182aed29a7ddd2e54353322cfb30abb9e3a6df2ac2c20fe23436311d678564d0c8d305930575f60e2d3d048184d79");
     ASSERT_EQ(sizeof(hash("sha512", "12345")), 128);
+#endif
 }
