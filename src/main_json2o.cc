7c5d693f2b74f44353deee57c48c23c84d9e6075
Yucong Sun <sunyucong@gmail.com>
2023-12-19 19:18:16 -0800

    Support stdin and stdout for o2json/json2o

--- src/main_json2o.cc
+++ src/main_json2o.cc
@@ -18,8 +18,8 @@ int main(int argc, char** argv) {
 
   argparse::ArgumentParser program("json2o");
 
-  program.add_argument("json_file").help("json file to write");
-  program.add_argument("o_file").help("o file to read");
+  program.add_argument("json_file").help("json file to read, if - then stdin is used");
+  program.add_argument("o_file").default_value("-").help("o file to read, if omitted, then stdout is used");
 
   try {
     program.parse_args(argc, argv);
@@ -30,26 +30,38 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  auto json_file = program.get("json_file");
-  std::ifstream ifs(json_file, std::ios::binary);
-  if (!ifs) {
-          std::cerr << "Error: cannot open file " << json_file << std::endl;
-          return 1;
-  }
   std::stringstream s;
-  s << ifs.rdbuf();
+
+  auto json_file = program.get("json_file");
+  if (json_file == "-") {
+    s << std::cin.rdbuf();
+  } else {
+    std::ifstream ifs(json_file, std::ios::binary);
+    if (!ifs) {
+      std::cerr << "Error: cannot open file " << json_file << std::endl;
+      return 1;
+    }
+    s << ifs.rdbuf();
+  }
+
   std::string content = s.str();
   if (content.empty()) {
     std::cerr << "Error: empty json file " << json_file << std::endl;
     return 1;
   }
+
   try {
     nlohmann::json j = nlohmann::json::parse(content);
     OFile obj(j);
     std::string result = obj.to_ofile();
-    std::ofstream ofs(program.get("o_file"), std::ios::binary);
-    ofs << result;
-    ofs.close();
+    auto o_file = program.get("o_file");
+    if (o_file == "-") {
+      std::cout << result;
+    } else {
+      std::ofstream ofs(o_file, std::ios::binary);
+      ofs << result;
+      ofs.close();
+    }
   } catch (const std::exception& err) {
     std::cerr << "Error: " << err.what() << std::endl;
     return 1;
