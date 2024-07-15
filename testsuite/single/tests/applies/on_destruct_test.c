29089e0033d928988ae4d482422935e8ea1976df
gesslar <karahd@gmail.com>
2024-07-15 11:02:43 -0400

    moving test objects to /single. maye this was causing the compile check issues?

--- testsuite/single/tests/applies/on_destruct_test.c
+++ testsuite/single/tests/applies/on_destruct_test.c
@@ -2,11 +2,10 @@ void do_tests()
 {
     object ob ;
 
-    ASSERT(ob = load_object(__DIR__"on_destruct_good")) ;
+    ASSERT(ob = load_object("/single/on_destruct_good")) ;
     catch(destruct(ob)) ;
     ASSERT_EQ(0, objectp(ob)) ;
 
-    ASSERT(ob = load_object(__DIR__"on_destruct_bad"))
     catch(destruct(ob)) ;
     ASSERT_EQ(0, objectp(ob)) ;
 }
