5cbbe13bfbf6ffb3d40fde4cfd11c370075845e9
gesslar <karahd@gmail.com>
2024-07-17 15:39:03 -0400

    missing newline in debug_message. adding in

--- src/vm/internal/base/object.cc
+++ src/vm/internal/base/object.cc
@@ -230,7 +230,7 @@ void save_svalue(svalue_t *v, char **buf) {
       // ignored
       break;
     default:
-      debug_message("save_svalue: unknown type %d ignored.", v->type);
+      debug_message("save_svalue: unknown type %d ignored.\n", v->type);
   }
 }
 
