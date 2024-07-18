f6a2e584b1262a187d4a3b3786f4779ae6967f2a
gesslar <karahd@gmail.com>
2024-07-17 23:42:19 -0400

    adding test in sprintf for 0-size buffer and adding crasher tests

---
+++ testsuite/single/tests/crasher/17.c
@@ -0,0 +1,4 @@
+void do_tests() {
+    debug_message("printf with allocate_buffer(0)") ;
+    printf("%O\n", allocate_buffer(0)) ;
+}
---
+++ testsuite/single/tests/crasher/17b.c
@@ -0,0 +1,4 @@
+void do_tests() {
+    debug_message("sprintf with allocate_buffer(0)") ;
+    debug_message(sprintf("%O", allocate_buffer(0))) ;
+}
