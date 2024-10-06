ef81453786640c634f85cd6137fefe3028e617ae
icewolfz <icewolfz@users.noreply.github.com>
2024-10-06 03:35:35 -0500

    Fix function prototype

--- src/comm.h
+++ src/comm.h
@@ -73,6 +73,6 @@ void on_user_logon(interactive_t *);
 int cmd_in_buf(interactive_t *ip);
 void on_user_input(interactive_t *ip, const char *data, size_t len);
 void on_user_websocket_received(interactive_t *ip, const char *data, size_t len);
-void on_user_websocket_binary_received(interactive_t *ip, const char *data, size_t len);
+void on_user_websocket_telnet_received(interactive_t *ip, const char *data, size_t len);
 
 #endif /* COMM_H */
