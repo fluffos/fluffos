db38b5e2d730efee06a723cb1f8844d7a2f030f7
Brian M. Workman <1266935+gesslar@users.noreply.github.com>
2025-11-12 20:08:57 -0500

    first-class-functions

--- docs/lpc/constructs/function.md
+++ docs/lpc/constructs/function.md
@@ -9,7 +9,7 @@ title: constructs / function
 The LPC function is similar but not identical to that provided by C
 (it is most similar to that provided by ANSI C). The syntax is as follows:
 
-```
+```c
 return_type function_name(arg1_type arg1, arg2_type arg2, ...)
 {
     variable_declarations;
@@ -27,7 +27,7 @@ Note that var0 must be of return_type.
 If a function doesn't need to return a value, then it should be declared
 with a return_type of "void". E.g.
 
-```
+```c
 void function_name(arg1_type arg1, ...)
 {
     statements;
@@ -46,3 +46,70 @@ You may invoke a function in another object as follows:
 or:
 
     call_other(object, function_name, arg1, arg2, ...);
+
+### First-Class Functions
+
+Functions can be treated as first-class values and stored in variables. You can
+use function variables to implement callbacks, strategy patterns, and functional
+programming techniques.
+
+**Storing functions:**
+
+```c
+function f = add;  // Store function reference
+int result = f(5, 3);  // Call it later
+```
+
+**Passing functions as arguments:**
+
+```c
+void apply_operation(int a, int b, function op) {
+    int result = op(a, b);
+    printf("Result: %d\n", result);
+}
+
+int add(int x, int y) { return x + y; }
+int multiply(int x, int y) { return x * y; }
+
+void create() {
+    apply_operation(5, 3, add);       // Result: 8
+    apply_operation(5, 3, multiply);  // Result: 15
+}
+```
+
+**Returning functions:**
+
+```c
+function get_comparator(string type) {
+    if (type == "ascending") return (: $1 - $2 :);
+    if (type == "descending") return (: $2 - $1 :);
+    return 0;
+}
+
+void sort_by_type(int *arr, string type) {
+    function cmp = get_comparator(type);
+    return sort_array(arr, cmp);
+}
+```
+
+**Storing in data structures:**
+
+```c
+// Array of functions
+function *operations = ({ add, subtract, multiply, divide });
+int result = operations[0](10, 5);
+
+// Mapping of functions
+mapping commands = ([
+    "attack": (: do_attack :),
+    "defend": (: do_defend :),
+    "heal": (: do_heal :),
+]);
+
+void execute_command(string cmd) {
+    function handler = commands[cmd];
+    if (handler) handler();
+}
+```
+
+See also: `types/function` for more details on function pointers and syntax.
---
+++ testsuite/single/tests/compiler/firstclass_functions.c
@@ -0,0 +1,158 @@
+inherit "/inherit/tests";
+
+// Test functions
+int add(int a, int b) {
+  return a + b;
+}
+
+int multiply(int a, int b) {
+  return a * b;
+}
+
+string concat(string a, string b) {
+  return a + b;
+}
+
+int identity(int x) {
+  return x;
+}
+
+// Test with arrays
+int *double_array(int *arr) {
+  return map(arr, (: $1 * 2 :));
+}
+
+// Global function variable
+function global_func;
+
+void test_basic_assignment() {
+  function f;
+  
+  // Test assigning local function to variable
+  f = add;
+  ASSERT_EQ(8, f(5, 3));
+  
+  f = multiply;
+  ASSERT_EQ(15, f(5, 3));
+  
+  // Test with string return type
+  function str_f = concat;
+  ASSERT_EQ("hello world", str_f("hello ", "world"));
+}
+
+void test_direct_invocation() {
+  function f1 = add;
+  function f2 = multiply;
+  
+  // Direct invocation should work
+  ASSERT_EQ(8, f1(5, 3));
+  ASSERT_EQ(15, f2(5, 3));
+  
+  // Should still work with evaluate()
+  ASSERT_EQ(8, evaluate(f1, 5, 3));
+  ASSERT_EQ(15, evaluate(f2, 5, 3));
+}
+
+void test_array_functions() {
+  int *nums = ({ 1, 2, 3, 4, 5 });
+  function f = double_array;
+  
+  int *result = f(nums);
+  ASSERT_EQ(({ 2, 4, 6, 8, 10 }), result);
+}
+
+void test_global_variable() {
+  // Assign to global function variable
+  global_func = add;
+  ASSERT_EQ(10, global_func(6, 4));
+  
+  global_func = multiply;
+  ASSERT_EQ(24, global_func(6, 4));
+}
+
+void test_function_in_array() {
+  function *funcs = ({ add, multiply });
+  
+  ASSERT_EQ(7, funcs[0](3, 4));
+  ASSERT_EQ(12, funcs[1](3, 4));
+}
+
+void test_function_in_mapping() {
+  mapping ops = ([
+    "add": add,
+    "multiply": multiply,
+  ]);
+  
+  ASSERT_EQ(9, ops["add"](4, 5));
+  ASSERT_EQ(20, ops["multiply"](4, 5));
+}
+
+void test_passing_to_higher_order() {
+  int *nums = ({ 1, 2, 3, 4, 5 });
+  
+  // Pass bare function name to map
+  int *doubled = map(nums, (: $(identity)($1 * 2) :));
+  ASSERT_EQ(({ 2, 4, 6, 8, 10 }), doubled);
+}
+
+void test_efun_pointer() {
+  // Test with efuns
+  function f = abs;
+  ASSERT_EQ(5, f(-5));
+  ASSERT_EQ(10, f(10));
+}
+
+void test_mixed_usage() {
+  // Test mixing old and new syntax
+  function f1 = add;                    // new syntax
+  function f2 = (: multiply :);         // old syntax
+  
+  ASSERT_EQ(8, f1(5, 3));              // new invocation
+  ASSERT_EQ(15, evaluate(f2, 5, 3));   // old invocation
+  
+  // Both should work with both invocation styles
+  ASSERT_EQ(8, evaluate(f1, 5, 3));
+  ASSERT_EQ(15, f2(5, 3));
+}
+
+void test_forward_declaration() {
+  // This tests that forward-declared functions work
+  function f = helper_function;
+  ASSERT_EQ(42, f(40));
+}
+
+int helper_function(int x) {
+  return x + 2;
+}
+
+void test_return_function() {
+  function f = get_adder();
+  ASSERT_EQ(11, f(5, 6));
+}
+
+function get_adder() {
+  return add;
+}
+
+void test_equivalence() {
+  // Verify that bare name creates same function pointer as (: name :)
+  function f1 = add;
+  function f2 = (: add :);
+  
+  ASSERT_EQ(evaluate(f1, 3, 4), evaluate(f2, 3, 4));
+}
+
+void do_tests() {
+  test_basic_assignment();
+  test_direct_invocation();
+  test_array_functions();
+  test_global_variable();
+  test_function_in_array();
+  test_function_in_mapping();
+  test_passing_to_higher_order();
+  test_efun_pointer();
+  test_mixed_usage();
+  test_forward_declaration();
+  test_return_function();
+  test_equivalence();
+}
